3ae10d3335a82c9d787f75fe6533685d9201e4b9
ahmeeabdi6-beep <ahmeeabdi6@gmail.com>
2026-07-16 22:08:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
-from flask import Flask
+import os
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
 
-app = Flask(__name__)
+TOKEN = os.getenv("BOT_TOKEN")
 
-@app.route("/")
-def home():
-    return "Ahmedalajami Bot is running successfully!"
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "🌹 Baga nagaan dhuftan gara Ahmedalajami Bot!\n\n"
+        "📚 Channelota Barnootaa\n"
+        "🎓 Barnoota fi beekumsa argachuuf nu hordofaa."
+    )
+
+def main():
+    app = Application.builder().token(TOKEN).build()
+
+    app.add_handler(CommandHandler("start", start))
+
+    print("Ahmedalajami Bot hojii jalqabe...")
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
